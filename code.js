5c7f5a88ff9ba9f7e2b9689b05f9ee0b5a66a443
isaia <imuckerheide@my.wctc.edu>
2020-10-26 21:32:17 -0700

    Initial commit

---
+++ code.js
@@ -0,0 +1,14 @@
+function firstButton()
+{
+
+
+        countClicks++;
+
+
+}
+var countClicks = 0;
+function secondButton()
+{
+alert(`You clicked the "Click me aye" button ${countClicks} times.`);
+    alert("Thank you for clicking my buttons.");
+}
